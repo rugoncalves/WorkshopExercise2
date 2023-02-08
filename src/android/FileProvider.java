afee7d926daaea48da7c117d02020ec72848da0c
rug <rmfgoncalves@gmail.com>
2023-02-08 17:15:49 +0100

    Update FileProvider.java

--- src/android/FileProvider.java
+++ src/android/FileProvider.java
@@ -3,5 +3,5 @@ package com.outsystems.documentpreview;
 /*
  * http://stackoverflow.com/questions/40746144/error-with-duplicated-fileprovider-in-manifest-xml-with-cordova/41550634#41550634
  */
-public class FileProvider extends android.support.v4.content.FileProvider {
+public class FileProvider extends androidx.core.content.FileProvider {
 }
